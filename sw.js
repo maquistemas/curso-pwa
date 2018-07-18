1f379511801864c66abf237ad9fb82086f6113ac
john <maquistemas@gmail.com>
2018-07-18 12:52:57 -0500

    serviceWorker

---
+++ sw.js
@@ -0,0 +1,9 @@
+/**
+ * El Service Worker nos permite trabajar la aplicación
+ * Offline. Pero la caché almacenada de detectar cambios
+ * en la web original. Traerá esos cambios.
+ * 
+ *      Service Worker
+ */
+
+ 
