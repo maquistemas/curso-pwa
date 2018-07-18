485d2e4b3adb0c925ffc04b848d64958a81dcf8e
john <maquistemas@gmail.com>
2018-07-18 11:41:05 -0500

    Scroll suavizado

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $("#menu a").click(function(e){
+        e.preventDefault();
+
+        
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        });
+
+        return false;
+    });
+
+});
